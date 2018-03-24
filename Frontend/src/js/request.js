313a18dfcae48a51197abdcd17446c6b29bf26c5
kanishk307 <ysj30797.kj@gmail.com>
2018-03-24 21:34:55 +0530

    js

--- Frontend/src/js/request.js
+++ Frontend/src/js/request.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
                 "address": $('#AddressWomen').val(),
                 "district": $('#DistrictWoman').val(),
                 "state": $('#StateWoman').val(),
-                "pincode": $('PincodeWoman').val()
+                "pincode": $('PincodeWoman').val(),
             },
             type: 'POST',
             jsonpCallback: 'callback', // this is not relevant to the POST anymore
@@ -30,39 +30,35 @@ $(document).ready(function() {
         });
     });
 
+   $('#FormCompany').submit(function(event){
+    
+        event.preventDefault();
+        console.log("Ajax called?");   
+        $.ajax({
+            url: 'http://localhost:8000/user/signup',
+            dataType:JSON,
+            data: {
+                "name":$('#NameEnterprise').val(),
+                "email":$('#EmailEnterprise').val(),
+                "address":$('#AddressEnterprise').val(),
+                "state":$('#StateEnterprise').val(),
+                "district":$('#DistrictEnterprise').val(),
+                "pincode":$('#PincodeEnterprise').val(),
+                "enterprise":$('#Enterprise').val()
+
+            },
+            type: 'POST',
+            jsonpCallback: 'callback', // this is not relevant to the POST anymore
+            success: function(data) {
+                // var ret = jQuery.parseJSON(data);
+                //            $('#women-signup').html(ret.msg);
+                console.log(data)
+            },
+            error: function(xhr, status, error) {
+                console.log('Error: ' + status + ' ' + error.message);
+            }
+        });
+   }); 
+
 });
 
-
-
-//    $(function() {
-//       
-//    $("#FormWomen").submit(function(event) {
-//
-//      
-//      event.preventDefault();
-//
-//     
-//      var $form = $( this ),
-//          url = $form.attr( 'action' );
-//
-//      
-//      var posting = $.post( url, { 
-//          name: $('#NameWoman').val(), 
-//          age: $('#AgeWoman').val(),
-//          address: $('#AddressWomen').val(),
-//          district:('#DistrictWoman').val(),
-//          state:$('#StateWoman').val(),
-//          pincode:$('PincodeWoman').val(),
-//      
-//      });
-//
-//      
-//      posting.done(function( data ) {
-//        alert('success');
-//      });
-//    });
-//        
-//        });        
-//        
-//       });
-//     });
